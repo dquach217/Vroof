a28165ce727f3e5bd51567ca9e4cb5723b2a6c39
dquach217 <dq6180@rit.edu>
2019-02-23 13:08:19 -0500

    init Portal

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float rotate_speed;
+    public float translate_speed;
+
+	void FixedUpdate () {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        float mouseInput = Input.GetAxis("Mouse X");
+
+        Vector3 moveHorizontal = transform.right * horizontalInput;
+        Vector3 moveVertical = transform.forward * verticalInput;
+
+        Vector3 move = (moveHorizontal + moveVertical).normalized * translate_speed * Time.fixedDeltaTime;
+
+        Vector3 rotation = new Vector3(0f, mouseInput, 0f) * rotate_speed;
+
+        transform.position += move;
+        transform.Rotate(rotation);
+
+    }
+}
---
+++ Assets/Scripts/PortalCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour {
+
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+
+	void Update () {
+
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angualarDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angualarDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+	}
+}
